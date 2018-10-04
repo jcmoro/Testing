39cb4c005f46e6908d0d6a20f84dac6c043197a7
José Carlos Moro Díaz <jcmorodiaz@gmail.com>
2018-10-04 10:20:53 +0200

    Create VideoTest.php

---
+++ VideoTest.php
@@ -0,0 +1,87 @@
+use PHPUnit\Framework\TestCase;
+use Video;
+
+class VideoTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function shouldCreateVideoWithTrueAutoplay()
+    {
+        $video = new Video();
+        $video->changeAutoplay(true);
+        static::assertTrue($video->autoplay());
+        static::assertArrayHasKey('autoplay', $video->toArray());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldCreateVideoWithFalseAutoplay()
+    {
+        $video = new Video();
+        static::assertFalse($video->autoplay());
+        static::assertArrayHasKey('autoplay', $video->toArray());
+    }
+
+    /**
+     * @test
+     * @dataProvider invalidBooleanData
+     * @expectedException \InvalidArgumentException
+     *
+     * @param mixed $data
+     */
+    public function invalidCreateVideoDataThrowsException($data)
+    {
+        $video = new Video();
+        $video->changeAutoplay($data);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldCreateVideoWithFalsePlayInSitu()
+    {
+        $video = new Video();
+        static::assertFalse($video->playInSitu());
+        static::assertArrayHasKey('playInSitu', $video->toArray());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldCreateVideoWithTruePlayInSitu()
+    {
+        $video = new Video(true);
+        static::assertTrue($video->playInSitu());
+        static::assertArrayHasKey('playInSitu', $video->toArray());
+    }
+
+    /**
+     * @test
+     * @dataProvider invalidBooleanData
+     * @expectedException \InvalidArgumentException
+     *
+     * @param mixed $playInSitu
+     */
+    public function invalidCreateVideoPlayInSituThrowsException($playInSitu)
+    {
+        new Video($playInSitu);
+    }
+
+    /**
+     * @return \Traversable
+     */
+    public function invalidBooleanData()
+    {
+        yield ['true'];
+        yield ['false'];
+        yield [0];
+        yield [126];
+        yield [0.0];
+        yield [126.1231];
+        yield [[]];
+        yield [null];
+        yield [\stdClass::class];
+    }
+}
